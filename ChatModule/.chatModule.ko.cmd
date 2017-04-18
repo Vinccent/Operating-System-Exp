e8a71b781379292ae71c1fb09edf8d93fc454fde
HoboChen <hobochen96@live.com>
2017-04-18 00:31:23 -0700

    rm hiding middle files

--- ChatModule/.chatModule.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hobochen/caesium/cae/chatModule.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hobochen/caesium/cae/chatModule.ko /home/hobochen/caesium/cae/chatModule.o /home/hobochen/caesium/cae/chatModule.mod.o
